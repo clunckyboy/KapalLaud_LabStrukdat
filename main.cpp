1a851cb3a183c8776c08381a6b651eeacf9b17fd
Edwin <edwinjonatanpurba@gmail.com>
2024-12-08 22:07:58 +0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int opsi;
+
+   while (true) {
+      system("cls");
+      cout << "=============================================================================" << endl;
+      cout << "                                 TOKO ONLINE                                 " << endl;
+      cout << "=============================================================================" << endl;
+      cout << "0. Keluar                                                                    " << endl;
+      cout << "1. Sortir Produk Berdasarkan Kategori dan Harga (Sorting & Hash)             " << endl;
+      cout << "2. Menambahkan Produk ke Keranjang Belanja (Array)                           " << endl;
+      cout << "3. Mencari Produk Berdasarkan ID (Search & Hash)                             " << endl;
+      cout << "4. Melihat Daftar Barang di Keranjang (Array)                                " << endl;
+      cout << "5. Menghapus Barang dari Keranjang (Array)                                   " << endl;
+      cout << "6. Menghitung Biaya Pengiriman Berdasarkan Jarak (Graph - Shortest Path)     " << endl; 
+      cout << "7. Melakukan Pembayaran dan Menyimpan Riwayat Pembayaran (Array)             " << endl;
+      cout << "8. Melihat Riwayat Pembayaran (Array)                                        " << endl;
+      cout << "9. Melihat Detail Setiap Transaksi pada Riwayat Pembayaran (Array & Hash)    " << endl;
+      cout << "=============================================================================" << endl;
+      cout << "Pilih opsi (0-9) : "; cin >> opsi;
+          
+   }
+}
